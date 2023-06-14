46a9a8493711db1b90e7a8920da1f492da5cfc4e
MisSero <misha555835@gmail.com>
2023-06-14 17:40:38 +0300

    add Model/TaskFormObject.cs

---
+++ Pomodoro Manager/Model/TaskFormObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Pomodoro_Manager.Model
+{
+    public class TaskFormObject
+    {
+        public string Name { get; set; }
+        public int PomodoroCounter { get; set; }
+
+        public TaskFormObject(string name, int pomodoroCounter)
+        {
+            Name = name;
+            PomodoroCounter = pomodoroCounter;
+        }
+    }
+}
