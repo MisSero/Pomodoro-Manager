7d7ef2892c56825c91d90d10af2afddc4618e874
MisSero <misha555835@gmail.com>
2023-06-18 19:50:43 +0300

    change properties in TaskController.cs

--- Pomodoro Manager/TaskController.cs
+++ Pomodoro Manager/TaskController.cs
@@ -3,26 +3,30 @@ namespace Pomodoro_Manager
 {
     public static class TaskController
     {
-        public static void AddToPanel(TaskFormObject task, Panel panel)
+        public static void AddToPanel(TaskFormObject task, Panel panel, Form1 form)
         {
             int panelTaskWidth = panel.Width - 30;
-            int panelTaskHeight = 35;
+            int panelTaskHeight = 33;
 
             Label taskLabel = new Label();
             taskLabel.Text = task.Name;
-            taskLabel.TextAlign = ContentAlignment.MiddleCenter;
-            taskLabel.Dock = DockStyle.Left;
+            taskLabel.AutoSize = true;
+            taskLabel.Font = new Font("Malgun Gothic", 12);
+            taskLabel.TextAlign = ContentAlignment.MiddleLeft;
 
             Button button = new Button();
-            button.Text = "button text";
+            button.Text = "Play!";
             button.Dock = DockStyle.Right;
+            button.Click += form.button1_Click;
+
 
             Panel taskPanel = new Panel();
             taskPanel.Size = new Size(panelTaskWidth, panelTaskHeight);
+            taskPanel.Padding = new Padding(1);
             taskPanel.Anchor = AnchorStyles.Left | AnchorStyles.Top;
             taskPanel.BorderStyle = BorderStyle.FixedSingle;
-            taskPanel.Controls.Add(taskLabel);
             taskPanel.Controls.Add(button);
+            taskPanel.Controls.Add(taskLabel);
             
             panel.Controls.Add(taskPanel);
         }
