f45ef432009382e18b68fbfd3b3c9939ccdb1e32
MisSero <misha555835@gmail.com>
2023-06-23 18:53:52 +0300

    fix Model/TaskFormObject: update properties

--- Pomodoro Manager/Model/TaskFormObject.cs
+++ Pomodoro Manager/Model/TaskFormObject.cs
@@ -2,13 +2,38 @@
 {
     public class TaskFormObject
     {
+        private int _goalCounter;
+        private int _currentCounter;
+        private string _displayCounter;
         public string Name { get; set; }
-        public int PomodoroCounter { get; set; }
+        public int GoalCounter
+        {
+            get => _goalCounter;
+            set
+            {
+                _goalCounter = value;
+                DisplayCounter = "";
+            }
+        }
+        public int CurrentCounter
+        {
+            get => _currentCounter;
+            set
+            {
+                _currentCounter = value;
+                DisplayCounter = "";
+            }
+        }
+        public string DisplayCounter
+        {
+            get => _displayCounter;
+            set => _displayCounter = $"{CurrentCounter}/{GoalCounter}";
+        }
 
         public TaskFormObject(string name, int pomodoroCounter)
         {
             Name = name;
-            PomodoroCounter = pomodoroCounter;
+            GoalCounter = pomodoroCounter;
         }
     }
 }
