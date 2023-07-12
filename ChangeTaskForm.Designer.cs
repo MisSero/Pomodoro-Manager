3b64a225e3afff6e1509252c7cc1bd1f525f0931
MisSero <misha555835@gmail.com>
2023-07-12 17:38:24 +0300

    add ChangeTaskFrom: form to change name and goal counter of task

---
+++ ChangeTaskForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Pomodoro_Manager
+{
+    partial class ChangeTaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ChangeTextBox = new TextBox();
+            ChangeNumericUpDown = new NumericUpDown();
+            SaveChangeButton = new Button();
+            CanсelChangeButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)ChangeNumericUpDown).BeginInit();
+            SuspendLayout();
+            // 
+            // ChangeTextBox
+            // 
+            ChangeTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            ChangeTextBox.BackColor = SystemColors.ActiveBorder;
+            ChangeTextBox.BorderStyle = BorderStyle.FixedSingle;
+            ChangeTextBox.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            ChangeTextBox.ForeColor = Color.Black;
+            ChangeTextBox.Location = new Point(12, 90);
+            ChangeTextBox.MaxLength = 60;
+            ChangeTextBox.Name = "ChangeTextBox";
+            ChangeTextBox.Size = new Size(344, 32);
+            ChangeTextBox.TabIndex = 4;
+            ChangeTextBox.Text = "TaskName";
+            // 
+            // ChangeNumericUpDown
+            // 
+            ChangeNumericUpDown.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            ChangeNumericUpDown.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            ChangeNumericUpDown.Location = new Point(362, 92);
+            ChangeNumericUpDown.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
+            ChangeNumericUpDown.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            ChangeNumericUpDown.Name = "ChangeNumericUpDown";
+            ChangeNumericUpDown.Size = new Size(86, 30);
+            ChangeNumericUpDown.TabIndex = 5;
+            ChangeNumericUpDown.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // SaveChangeButton
+            // 
+            SaveChangeButton.Anchor = AnchorStyles.Right;
+            SaveChangeButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            SaveChangeButton.Location = new Point(278, 146);
+            SaveChangeButton.Name = "SaveChangeButton";
+            SaveChangeButton.Size = new Size(170, 34);
+            SaveChangeButton.TabIndex = 6;
+            SaveChangeButton.Text = "Save";
+            SaveChangeButton.UseVisualStyleBackColor = true;
+            SaveChangeButton.Click += SaveChangeButton_Click;
+            // 
+            // CanсelChangeButton
+            // 
+            CanсelChangeButton.Anchor = AnchorStyles.Left;
+            CanсelChangeButton.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            CanсelChangeButton.Location = new Point(12, 146);
+            CanсelChangeButton.Name = "CanсelChangeButton";
+            CanсelChangeButton.Size = new Size(170, 34);
+            CanсelChangeButton.TabIndex = 7;
+            CanсelChangeButton.Text = "Canсel";
+            CanсelChangeButton.UseVisualStyleBackColor = true;
+            CanсelChangeButton.Click += CanсelChangeButton_Click;
+            // 
+            // ChangeTaskForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkGray;
+            ClientSize = new Size(460, 254);
+            Controls.Add(CanсelChangeButton);
+            Controls.Add(SaveChangeButton);
+            Controls.Add(ChangeNumericUpDown);
+            Controls.Add(ChangeTextBox);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ChangeTaskForm";
+            Text = "ChangeTaskForm";
+            ((System.ComponentModel.ISupportInitialize)ChangeNumericUpDown).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox ChangeTextBox;
+        private NumericUpDown ChangeNumericUpDown;
+        private Button SaveChangeButton;
+        private Button CanсelChangeButton;
+    }
+}
---
+++ ChangeTaskForm.cs
@@ -0,0 +1,32 @@
+﻿using Pomodoro_Manager.Model;
+using Pomodoro_Manager.ViewModel;
+namespace Pomodoro_Manager
+{
+    public partial class ChangeTaskForm : Form
+    {
+        private TaskFormObject _task;
+        private InputPanelController _inputPanelController;
+        public ChangeTaskForm(TaskFormObject task)
+        {
+            InitializeComponent();
+
+            _inputPanelController = new InputPanelController(ChangeNumericUpDown);
+            _task = task;
+
+            ChangeTextBox.Text = _task.Name;
+            ChangeNumericUpDown.Value = _task.GoalCounter;
+        }
+
+        private void CanсelChangeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveChangeButton_Click(object sender, EventArgs e)
+        {
+            _task.Name = ChangeTextBox.Text;
+            _task.GoalCounter = (int)ChangeNumericUpDown.Value;
+            this.Close();
+        }
+    }
+}
